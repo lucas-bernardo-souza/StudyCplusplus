e6b278ee3255c1d2c0934d3287418e7ac8448f3d
Lucas Bernardo de Souza <lucasbernardodesouza510@gmail.com>
2023-02-09 10:03:53 -0300

    Constantes e declaracoes multiplas de variaveis

---
+++ Constantes_DeclaracoesMultiplasVariaveis/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// declaração de constantes de forma global
+#define pi 3.1415
+// com a diretiva define e possivel atribuir comandos tambem
+#define printaNome cout << "Lucas Bernardo de Souza" << endl;
+
+int main() {
+
+    // declaração multipla
+    int vidas=3, tiros=500, life=100;
+     cout << pi << endl;
+     printaNome
+
+
+    system("pause");
+    return 0;
+}
